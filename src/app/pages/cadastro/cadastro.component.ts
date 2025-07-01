d8cf14dbd88fd30813c2ac92794ace36da3e9ebf
nicholas prado <nicholas.prado@vitalsis.com.br>
2025-07-01 11:11:41 -0300

    Atualização Cadastro

--- src/app/pages/cadastro/cadastro.component.ts
+++ src/app/pages/cadastro/cadastro.component.ts
@@ -1,47 +1,66 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { Router, RouterModule, RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { UsuarioService } from '../../services/usuario.service';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { FloatLabelModule } from 'primeng/floatlabel';
 
 @Component({
   selector: 'app-cadastro',
-  imports: [CommonModule, ReactiveFormsModule, RouterModule, RouterLink, MatButtonModule, MatIconModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    RouterModule,
+    RouterLink,
+    MatButtonModule,
+    MatIconModule,
+    FloatLabelModule,
+  ],
   templateUrl: './cadastro.component.html',
-  styleUrl: './cadastro.component.scss'
+  styleUrl: './cadastro.component.scss',
 })
 export class CadastroComponent {
   cadastroForm: FormGroup;
 
-   constructor(private fb: FormBuilder, private usuarioService: UsuarioService, private router: Router) {
-    this.cadastroForm = this.fb.group({
-      nome: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
-      senha: ['', [Validators.required, Validators.minLength(6)]],
-      confirmarSenha: ['', Validators.required]
-    }, {
-      validators: this.validarSenhasIguais
-    });
-  }
+  constructor(
+    private fb: FormBuilder,
+    private usuarioService: UsuarioService,
+    private router: Router
+  ) {
+    this.cadastroForm = this.fb.group(
+      {
+        nome: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+        senha: ['', [Validators.required, Validators.minLength(6)]],
+      },
+    )
+  };
 
-  validarSenhasIguais(form: FormGroup) {
-    const senha = form.get('senha')?.value;
-    const confirmar = form.get('confirmarSenha')?.value;
-    return senha === confirmar ? null: { senhasDiferentes: true };
-  }
 
   onSubmit() {
     if (this.cadastroForm.valid) {
       const { nome, email, senha } = this.cadastroForm.value;
 
       this.usuarioService.cadastrar({ nome, email, senha }).subscribe(() => {
-      alert('Cadastro realizado com sucesso!'); // msg q aparece qnd o usuario aperta no bt cadastrar
-      this.router.navigate(['/']); // volta pra tela de login dps de cadastrar
+        alert('Cadastro realizado com sucesso!'); // msg q aparece qnd o usuario aperta no bt cadastrar
+        this.router.navigate(['']); // volta pra tela de login dps de cadastrar
       });
-    } else if (this.cadastroForm.hasError('senhasDiferentes')) {
-      alert('Senhas não são iguais.');
     }
+  }
+
+  voltarLogin() {
+    this.router.navigate(['']);
+  }
+
+  campoInvalido(campo: string): boolean {
+    const control = this.cadastroForm.get(campo);
+    return !!control && control.invalid && control.touched;
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
-import { routes } from './app/app.routes';
 import 'zone.js';
 import { appConfig } from './app/app.config';
 
